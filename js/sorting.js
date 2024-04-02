e2a404429eb9e04401bd7e18802321fa6d74bc5c
Punya Jayarajan <73706543+Punyaj@users.noreply.github.com>
2024-04-02 23:51:48 +0530

    Add files via upload

--- js/sorting.js
+++ js/sorting.js
@@ -6,28 +6,41 @@ let selectedCategory = initialSelected.innerText.toLowerCase(); // 'all'
 
 // Pictures
 const pictures = [
+	
+	{
+		url:
+			'./images/Calypso_gcek.png',
+		category: 'all',
+		file: 'https://calypso-gcek.github.io/calypsogcek/index.html'
+
+	},
+	{
+		url:
+			'./images/Meet_and_Greet.png',
+		category: 'all',
+		file: 'https://shegcek.github.io/meet-and-greet/'
+
+	},
+	{
+		url:
+			'./images/SHE_STATE_LEVEL_LAUNCH.png',
+		category: 'all',
+		file: 'https://shegcek.github.io/SHE-State-Level-Launch/'
+
+	},
 	{
 		url:
 			'./images/COWIKI.jpg',
-		category: 'all'
+		category: 'all',
+		file: 'https://punyaj.github.io/Covid-Encyclopedia/index.html'
 	},
 	{
 		url:
 			'./images/sanjeevani.jpg',
-		category: 'all'
+		category: 'all',
+		file: 'https://punyaj.github.io/ambulance-traffic-signal-alert/index.html'
 	},
-    {
-		url:
-			'./images/SHE_STATE_LEVEL_LAUNCH.png',
-		category: 'all'
-
-	},
-	{
-		url:
-			'./images/Calypso_gcek.png',
-		category: 'all'
-
-	},
+   
 	{
 		url:
 			'./images/DTMF.jpg',
@@ -38,30 +51,77 @@ const pictures = [
 			'./images/Mini-Project.jpeg',
 		category: 'all'
 	},
+
+	{
+		url: './images/TheHillMoviePoster.png',
+		category: 'all',
+		file: './images/TheHillMoviePoster.png',
+	},
+	{
+		url: './images/TheLegendMoviePoster.png',
+		category: 'all',
+		file: '../images/TheLegendMoviePoster.png',
+	},
+	{
+		url: './images/TheSpecialist MoviePoster.png',
+		category: 'all',
+		file: './images/TheSpecialist MoviePoster.png'
+	},
+
+
+
+
+
 	{
 		url:
-			'./images/COWIKI.jpg',
-		category: 'web'
+			'./images/Calypso_gcek.png',
+		category: 'web',
+		file: 'https://calypso-gcek.github.io/calypsogcek/index.html'
 
 	},
 	{
 		url:
-			'./images/sanjeevani.jpg',
-		category: 'web'
-        
+			'./images/Meet_and_Greet.png',
+		category: 'web',
+		file: 'https://shegcek.github.io/meet-and-greet/'
+
 	},
 	{
 		url:
 			'./images/SHE_STATE_LEVEL_LAUNCH.png',
-		category: 'web'
+		category: 'web',
+		file: 'https://shegcek.github.io/SHE-State-Level-Launch/'
 
 	},
 	{
 		url:
-			'./images/Calypso_gcek.png',
-		category: 'web'
-
+			'./images/COWIKI.jpg',
+		category: 'web',
+		file: 'https://punyaj.github.io/Covid-Encyclopedia/index.html'
 	},
+	{
+		url:
+			'./images/sanjeevani.jpg',
+		category: 'web',
+		file: 'https://punyaj.github.io/ambulance-traffic-signal-alert/index.html'
+	},
+   
+{
+	url: './images/TheHillMoviePoster.png',
+	category: 'poster',
+	file: './images/TheHillMoviePoster.png',
+},
+{
+	url: './images/TheLegendMoviePoster.png',
+	category: 'poster',
+	file: '../images/TheLegendMoviePoster.png',
+},
+{
+	url: './images/TheSpecialist MoviePoster.png',
+	category: 'poster',
+	file: './images/TheSpecialist MoviePoster.png'
+},
+
 	{
 		url:
 			'./images/DTMF.jpg',
@@ -74,16 +134,20 @@ const pictures = [
 	}
 ];
 
+
 // Render pictures based on category
 const renderPictures = category => {
-	pictures.forEach(picture => {
-		if (picture.category === category) {
-			const img = document.createElement('img');
-			img.src = picture.url;
-			img.srcset = picture.url;
-			grid.appendChild(img);
-		}
-	});
+    pictures.forEach(picture => {
+        if (picture.category === category) {
+            const img = document.createElement('img');
+            img.src = picture.url;
+            img.srcset = picture.url;
+            img.addEventListener('click', () => {
+                window.open(picture.file, '_blank'); // Open file in a new tab
+            });
+            grid.appendChild(img);
+        }
+    });
 };
 
 // Initial render
@@ -91,20 +155,20 @@ renderPictures(selectedCategory);
 
 // Menu & Filter
 categories.forEach(category => {
-	category.addEventListener('click', () => {
-		// Update selected category and add 'selected' class
-		selectedCategory = category.innerText.toLowerCase();
-		category.classList.add('selected');
+    category.addEventListener('click', () => {
+        // Update selected category and add 'selected' class
+        selectedCategory = category.innerText.toLowerCase();
+        category.classList.add('selected');
 
-		// Re-render pictures
-		grid.innerHTML = '';
-		renderPictures(selectedCategory);
+        // Re-render pictures
+        grid.innerHTML = '';
+        renderPictures(selectedCategory);
 
-		// Remove 'selected' class from the others
-		categories.forEach(categ => {
-			if (categ.innerText.toLowerCase() !== selectedCategory) {
-				categ.classList.remove('selected');
-			}
-		});
-	});
+        // Remove 'selected' class from the others
+        categories.forEach(categ => {
+            if (categ.innerText.toLowerCase() !== selectedCategory) {
+                categ.classList.remove('selected');
+            }
+        });
+    });
 });
